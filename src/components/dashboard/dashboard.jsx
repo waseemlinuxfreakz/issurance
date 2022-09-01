85a80102af28cccf7361b1615dc35aca3687c800
waseemlinuxfreakz <89018806+waseemlinuxfreakz@users.noreply.github.com>
2022-09-01 13:47:50 +0500

    all install new changes

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,5 +1,5 @@
 import { useState,React,Component,Link } from 'react';
-
+import { render } from "react-dom";
 import { ProSidebar, Menu, MenuItem, SubMenu,SidebarHeader,SidebarFooter,SidebarContent } from 'react-pro-sidebar';
 import 'react-pro-sidebar/dist/css/styles.css';
 import { FaList, FaRegHeart } from "react-icons/fa";
@@ -9,6 +9,40 @@ import { BiCog } from "react-icons/bi";
 import {headercss} from "../../header.css";
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
+import { Chart } from "react-google-charts";
+
+export const data = [
+    [
+      "Month",
+      "Bolivia",
+      "Ecuador",
+      "Madagascar",
+      "Papua New Guinea",
+      "Rwanda",
+      "Average",
+    ],
+    ["2004/05", 165, 938, 522, 998, 450, 614.6],
+    ["2005/06", 135, 1120, 599, 1268, 288, 682],
+    ["2006/07", 157, 1167, 587, 807, 397, 623],
+    ["2007/08", 139, 1110, 615, 968, 215, 609.4],
+    ["2008/09", 136, 691, 629, 1026, 366, 569.6],
+  ];
+
+  export const data_second = [
+    ["Element", "Density", { role: "style" }],
+    ["Copper", 8.94, "#b87333"], // RGB value
+    ["Silver", 10.49, "silver"], // English color name
+    ["Gold", 19.3, "gold"],
+    ["Platinum", 21.45, "color: #e5e4e2"], // CSS-style declaration
+  ];
+  
+  export const options = {
+    title: "Monthly Coffee Production by Country",
+    vAxis: { title: "Cups" },
+    hAxis: { title: "Month" },
+    seriesType: "bars",
+    series: { 5: { type: "line" } },
+  };
 
 function DashboardUser() {
 
@@ -78,10 +112,16 @@ function DashboardUser() {
                 className="mb-3"
                 >
                 <Tab eventKey="home" title="TODAY">
-                    Testing first
+                <Chart chartType="ColumnChart" width="100%" height="400px" data={data_second} />
                 </Tab>
                 <Tab eventKey="profile" title="LAST 7 DAYS">
-                    TESTING SECOND
+                <Chart
+      chartType="ComboChart"
+      width="100%"
+      height="400px"
+      data={data}
+      options={options}
+    />
                 </Tab>
                 <Tab eventKey="contact" title="LAST 30 DAYS">
                     Testing third
